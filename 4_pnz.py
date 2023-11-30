7be99e83739b8526c28f0fe5bb01014522540c4b
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-11-30 17:53:55 -0300

    aprendendo os conceitos do if , elif , else

--- 4_pnz.py
+++ 4_pnz.py
@@ -1,6 +1,6 @@
 #Verificação de Número Positivo, Negativo ou Zero:
 #Crie um programa que verifica se um número é positivo, negativo ou zero.
-
+#PNZ = Positivo , Negativo , Zero
 numero = float(input("Digite um número: "))
 
 if numero > 0:
