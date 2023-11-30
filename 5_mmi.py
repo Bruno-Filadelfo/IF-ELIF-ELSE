bd96b584542cf32a874d3da2a36fe8f3c21d140f
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-11-30 17:57:00 -0300

    aprendendo os conceitos do if , elif , else

--- 5_mmi.py
+++ 5_mmi.py
@@ -1,6 +1,6 @@
 #Verificação de Número Maior, Menor ou Igual:
 #Crie um programa que pede dois números ao usuário e informa se o primeiro número é maior, menor ou igual ao segundo número.
-
+#MMI = Maior , Menor e Igual  
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
