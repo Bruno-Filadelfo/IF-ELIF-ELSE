316a8d5d2ac5f646814dee5ff54a629ac0acc304
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-12-05 00:15:50 -0300

    conceitos da lógica do if , elif e else python.

---
+++ 7_Calculadorademaioridade.py
@@ -0,0 +1,10 @@
+#Calculadora de maioridade
+
+# Solicite a idade do usuário
+idade = int (input("Digite sua idade: "))
+
+# Verifique se o usuário é maior de idade
+if idade >=18:
+    print("Você é maior de idade")
+else:
+    print("Você é menor de idade")    
