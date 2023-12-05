dc0f9c6a976cbbd79c35deda74b91bfab567ec81
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-12-05 02:01:50 -0300

    conceitos da lógica do if , elif e else python.

---
+++ 9_Classificação_de_notas.py
@@ -0,0 +1,8 @@
+# Solicite a nota do aluno
+nota = float(input("Digite a nota do aluno: "))
+
+if nota >= 6.0:
+    print("Aluno está aprovado")
+elif nota <= 5.9:
+    print("Aluno ficara com recuperação")
+    
